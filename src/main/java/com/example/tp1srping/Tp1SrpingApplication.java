755085c5b2f37b987ad3c90aead293784c051a5d
Aldanari <curylorafael945@gmail.com>
2024-11-21 15:23:39 +0100

    Added hello_world

--- src/main/java/com/example/tp1srping/Tp1SrpingApplication.java
+++ src/main/java/com/example/tp1srping/Tp1SrpingApplication.java
@@ -2,12 +2,19 @@ package com.example.tp1srping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Tp1SrpingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp1SrpingApplication.class, args);
 	}
 
+	@GetMapping("/hello_world")
+	public String hello() {
+		return "Hello World";
+	}
 }
